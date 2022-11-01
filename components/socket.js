d7c7b5a233abbe56006be115343135300a9c3865
muhammadkh303@gmail.com <muhammadkh303@gmail.com>
2022-11-01 23:38:17 +0500

    port issue

--- components/socket.js
+++ components/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-export const socket = io('https://whatsapp-ba.herokuapp.com:3000',{ transports: ['websocket', 'polling', 'flashsocket'] });
+export const socket = io('https://whatsapp-ba.herokuapp.com:8000',{ transports: ['websocket', 'polling', 'flashsocket'] });
 export let socketID = '';
 socket.on('connect', () => {
     console.log(socket.id)
