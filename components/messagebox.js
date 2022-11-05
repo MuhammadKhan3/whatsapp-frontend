54e8262bc76d4b9928adfd8e0ea5ffa9b2ea36a5
muhammadkh303@gmail.com <muhammadkh303@gmail.com>
2022-11-05 11:54:41 +0500

    mut e reagain1

--- components/messagebox.js
+++ components/messagebox.js
@@ -141,7 +141,7 @@ const MessageBox = ({messagesEndRef,messages,user}) =>{
                           {video ?
                             <svg onClick={MuteVideohandler}  xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`bi bi-camera-video-fill ${Styles.videoIcon}`} viewBox="0 0 16 16"> <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z" fill="white"></path> </svg>
                             :
-                            <svg style={{color: 'red'}} onClick={unMuteVideohandler} xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`bi bi-camera-video-off-fill ${Styles.videMuteIcon}`} viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M10.961 12.365a1.99 1.99 0 0 0 .522-1.103l3.11 1.382A1 1 0 0 0 16 11.731V4.269a1 1 0 0 0-1.406-.913l-3.111 1.382A2 2 0 0 0 9.5 3H4.272l6.69 9.365zm-10.114-9A2.001 2.001 0 0 0 0 5v6a2 2 0 0 0 2 2h5.728L.847 3.366zm9.746 11.925-10-14 .814-.58 10 14-.814.58z" fill="red"></path> </svg>
+                            <svg style={{color: 'red'}} onClick={unMuteVideohandler} xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`bi bi-camera-video-off-fill ${Styles.videMuteIcon}`} viewBox="0 0 16 16"> <path fillRule="evenodd" d="M10.961 12.365a1.99 1.99 0 0 0 .522-1.103l3.11 1.382A1 1 0 0 0 16 11.731V4.269a1 1 0 0 0-1.406-.913l-3.111 1.382A2 2 0 0 0 9.5 3H4.272l6.69 9.365zm-10.114-9A2.001 2.001 0 0 0 0 5v6a2 2 0 0 0 2 2h5.728L.847 3.366zm9.746 11.925-10-14 .814-.58 10 14-.814.58z" fill="red"></path> </svg>
                           }
                           {/* red icon */}
                         </div>
